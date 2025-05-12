3b73a6b88972963018147c28ab068a109c1c8e46
mael-donnadille <mael.donnadille@e.rascol.net>
2025-05-12 10:49:00 +0200

    Front
    Contrôleur AngularJS

--- frontend/controllers/humidityController.js
+++ frontend/controllers/humidityController.js
@@ -5,15 +5,22 @@ app.controller('HumidityController', function($scope, HumidityService) {
     function loadHumidities() {
         HumidityService.getAll().then(function(response) {
             $scope.humidities = response.data;
+        }, function(error) {
+            console.error('Erreur lors de la récupération des humidités:', error);
         });
     }
+
     $scope.addHumidity = function() {
         if ($scope.newHumidity) {
             HumidityService.add($scope.newHumidity).then(function() {
                 loadHumidities();
                 $scope.newHumidity = '';
+            }, function(error) {
+                console.error('Erreur lors de l\'ajout de l\'humidité:', error);
             });
         }
     };
+
+    
     loadHumidities();
 });
